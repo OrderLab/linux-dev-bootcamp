4723e1f649e537b3cf93687d99472fa68fa17866
Ryan Huang <huang@cs.jhu.edu>
2021-06-05 05:45:59 -0400

    chroot mount script with mounting of device files and proc to enable apt-get like command in simple chroot command without booting the image with qemu

---
+++ chmount.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 IMAGE_FILE MOUNT_DIR"
+  exit 1
+fi
+
+image_file=$1
+mount_dir=$2
+
+if [[ $image_file != *.img ]]; then
+  echo "Image file $image_file does not end with .img"
+  exit 1
+fi
+
+if [ ! "$(ls -A $mount_dir)" ]; then
+  sudo mount -o loop  $image_file $mount_dir
+fi
+
+if [ ! "$(ls -A $mount_dir/proc)" ]; then
+  sudo mount -o bind,ro /dev $mount_dir/dev
+  sudo mount -o bind,ro /dev/pts $mount_dir/dev/pts
+  sudo mount -t proc none $mount_dir/proc
+else
+  echo "/proc, /dev already mounted to $mount_dir"
+fi
+
+sudo LANG=C.UTF-8 chroot $mount_dir /bin/bash
---
+++ chumount.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 IMAGE_FILE MOUNT_DIR"
+  exit 1
+fi
+
+image_file=$1
+mount_dir=$2
+
+if [[ $image_file != *.img ]]; then
+  echo "Image file $image_file does not end with .img"
+  exit 1
+fi
+
+if [ ! "$(ls -A $mount_dir)" ]; then
+  echo "Mount directory $mount_dir is empty"
+  exit 0
+fi
+
+sudo umount $mount_dir/dev/pts
+sudo umount $mount_dir/dev
+sudo umount $mount_dir/proc
+sudo umount $mount_dir
